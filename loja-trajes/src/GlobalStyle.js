3530acd9bb7eb111b4b0791e31b63e57e28cdfe8
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-12-09 14:37:48 -0300

    Projeto finalizado

---
+++ loja-trajes/src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Inter", sans-serif;
+
+}
+`;
+export default GlobalStyle
